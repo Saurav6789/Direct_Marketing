a2b9ad82634c65cf5c9f0e7e8e2a394291d95d24
saurav6789 <saurav9786@gmail.com>
2024-05-14 16:44:34 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='maximize the profit by marketing',
+    author='Saurav',
+    license='MIT',
+)
